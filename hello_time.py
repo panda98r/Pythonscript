22dea362ef54b855c6a4c6f0df829969ac8ec291
root <root@ip-172-31-1-253.ap-south-1.compute.internal>
2024-10-16 19:46:17 +0000

    first script

---
+++ hello_time.py
@@ -0,0 +1,7 @@
+# This script prints helloworld and current date and time
+import datetime
+
+current_time = datetime.datetime.now()
+print("Hello, World!")
+print("Current Date and Time:", current_time)
+
